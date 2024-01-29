f23996a6c05f7f27552089d84698e306eb981cee
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2024-01-29 07:17:57 +0000

    Added Ingress back

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
+import logging
 import uvicorn
-from fastapi import FastAPI
-from dotenv import load_dotenv
 from kubernetes import client, config
+from fastapi import FastAPI, HTTPException
 from starlette.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,34 +24,50 @@ async def connection_test():
 
 @app.delete("/delete_pod")
 async def delete_pod(uid: str):
-    config.load_incluster_config()
-    api_instance = client.AppsV1Api()
-    core_v1_api = client.CoreV1Api()
+    try:
+        config.load_incluster_config()
+        apps_v1_api = client.AppsV1Api()
+        core_v1_api = client.CoreV1Api()
+        networking_v1_api = client.NetworkingV1Api()
+        namespace = os.getenv("NAMESPACE")
 
-    api_response = api_instance.delete_namespaced_deployment(
-        name=f"deployment-{uid}",
-        namespace=os.getenv("NAMESPACE"),
-        body=client.V1DeleteOptions(
-            propagation_policy='Foreground',
+        # Delete Deployment
+        deployment_response = apps_v1_api.delete_namespaced_deployment(
+            name=f"deployment-{uid}",
+            namespace=namespace,
+            body=client.V1DeleteOptions(propagation_policy='Foreground')
         )
-    )
-    print(f"Deployment '{uid}' deleted. Status: {api_response.status}")
+        logging.info(f"Deployment '{uid}' deleted. Status: {deployment_response.status}")
 
-    api_response = core_v1_api.delete_namespaced_service(
-        name=f"service-{uid}",
-        namespace=os.getenv("NAMESPACE"),
-        body=client.V1DeleteOptions()
-    )
-    print(f"Service '{uid}' deleted. Status: {api_response.status}")
+        # Delete Service
+        service_response = core_v1_api.delete_namespaced_service(
+            name=f"service-{uid}",
+            namespace=namespace,
+            body=client.V1DeleteOptions()
+        )
+        logging.info(f"Service '{uid}' deleted. Status: {service_response.status}")
 
-    api_response = core_v1_api.delete_namespaced_secret(
-        name=f"secret-{uid}",
-        namespace=os.getenv("NAMESPACE"),
-        body=client.V1DeleteOptions()
-    )
-    print(f"Secret '{uid}' deleted. Status: {api_response.status}")
+        # Delete Secret
+        secret_response = core_v1_api.delete_namespaced_secret(
+            name=f"secret-{uid}",
+            namespace=namespace,
+            body=client.V1DeleteOptions()
+        )
+        logging.info(f"Secret '{uid}' deleted. Status: {secret_response.status}")
 
-    return JSONResponse(content="Deleted Pod Successfully!", status_code=200)
+        # Delete Ingress
+        ingress_response = networking_v1_api.delete_namespaced_ingress(
+            name=f"ingress-{uid}",
+            namespace=namespace,
+            body=client.V1DeleteOptions()
+        )
+        logging.info(f"Ingress '{uid}' deleted. Status: {ingress_response.status}")
+
+    except client.exceptions.ApiException as e:
+        logging.error(f"Error deleting Kubernetes resources: {e}")
+        raise HTTPException(status_code=500, detail=f"Error occurred while deleting Kubernetes resources for uid: {uid}")
+
+    return JSONResponse(content=f"Deleted pod and associated resources for '{uid}' successfully", status_code=200)
 
 
 if __name__ == '__main__':
